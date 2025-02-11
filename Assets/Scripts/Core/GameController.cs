58b1aa9e8376b85dc08f7f304fcfef244e8c8576
furkancanc <fcc.ceylan@gmail.com>
2025-02-11 22:50:41 +0300

    Referencing Other Game Objects

--- Assets/Scripts/Core/GameController.cs
+++ Assets/Scripts/Core/GameController.cs
@@ -5,4 +5,22 @@ public class GameController : MonoBehaviour
     Board gameBoard;
     Spawner spawner;
 
+    private void Start()
+    {
+        //gameBoard = GameObject.FindWithTag("Board").GetComponent<Board>();
+        //spawner = GameObject.FindWithTag("Spawner").GetComponent<Spawner>();
+        gameBoard = GameObject.FindFirstObjectByType<Board>();
+        spawner = GameObject.FindFirstObjectByType<Spawner>();
+
+        if (!gameBoard)
+        {
+            Debug.LogWarning("WARNING! There is no game board defined!");
+        }
+
+        if (!spawner)
+        {
+            Debug.LogWarning("WARNING! There is no spawner defined!");
+        }
+    }
+
 }
