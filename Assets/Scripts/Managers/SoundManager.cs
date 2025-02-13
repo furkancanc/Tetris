aaade2d31fa092803f60379cd2d86e5f94d6f728
furkancanc <fcc.ceylan@gmail.com>
2025-02-13 20:11:16 +0300

    Background Music

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -15,5 +15,67 @@ public class SoundManager : MonoBehaviour
     public AudioClip moveSound;
     public AudioClip dropSound;
     public AudioClip gameOverSound;
-    public AudioClip backgroundMusic;
+
+    public AudioSource musicSource;
+
+    public AudioClip[] musicClips;
+
+    AudioClip randomMusicClip;
+
+    private void Start()
+    {
+        randomMusicClip = GetRandomClip(musicClips);
+        PlayBackgroundMusic(randomMusicClip);
+    }
+
+    public AudioClip GetRandomClip(AudioClip[] clips)
+    {
+        AudioClip randomClip = clips[Random.Range(0, clips.Length)];
+        return randomClip;
+    }
+
+    public void PlayBackgroundMusic(AudioClip musicClip)
+    {
+        // Return if music is disabled or if musicSource is null or is musicClip is null
+        if (!musicEnabled || !musicClip || !musicSource)
+        {
+            return;
+        }
+
+        // if music is playing, stop it
+        musicSource.Stop();
+
+        musicSource.clip = musicClip;
+
+        // set the music volume
+        musicSource.volume = musicVolume;
+
+        // music repeats forever
+        musicSource.loop = true;
+
+        // start playing
+        musicSource.Play();
+    }
+
+    void UpdateMusic()
+    {
+        if (musicSource.isPlaying != musicEnabled)
+        {
+            if (musicEnabled)
+            {
+                randomMusicClip = GetRandomClip(musicClips);
+                PlayBackgroundMusic(randomMusicClip);
+            }
+            else
+            {
+                musicSource.Stop();
+            }
+        }
+    }
+
+    public void ToggleMusic()
+    {
+        musicEnabled = !musicEnabled;
+        UpdateMusic();
+    }
 }
