946daa35f9c897c69edc9c634a67be3ea9bd765f
furkancanc <fcc.ceylan@gmail.com>
2025-02-14 22:46:34 +0300

    Shape Queue (Part2)

--- Assets/Scripts/Core/Spawner.cs
+++ Assets/Scripts/Core/Spawner.cs
@@ -4,6 +4,9 @@ using Random = UnityEngine.Random;
 public class Spawner : MonoBehaviour
 {
     public Shape[] allShapes;
+    public Transform[] queuedXForms = new Transform[3];
+
+    Shape[] queuedShapes = new Shape[3];
 
     Shape GetRandomShape()
     {
@@ -34,5 +37,32 @@ public class Spawner : MonoBehaviour
             return null;
         }
 
+    }
+
+    void InitQueue()
+    {
+        for (int i = 0; i < queuedShapes.Length; ++i)
+        {
+            queuedShapes[i] = null;
+        }
+
+        FillQueue();
+    }
+
+    void FillQueue()
+    {
+        for (int i = 0; i < queuedShapes.Length; ++i)
+        {
+            if (!queuedShapes[i])
+            {
+                queuedShapes[i] = Instantiate(GetRandomShape(), transform.position, Quaternion.identity) as Shape;
+                queuedShapes[i].transform.position = queuedXForms[i].position;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        InitQueue();
     }
 }
