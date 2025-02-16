2b6116a43f092138e05f180b8390b1057fa17187
furkancanc <fcc.ceylan@gmail.com>
2025-02-16 22:10:09 +0300

    The Particle Player Class

---
+++ Assets/Scripts/Utility/ParticlePlayer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ParticlePlayer : MonoBehaviour
+{
+    public ParticleSystem[] allParticles;
+
+    private void Start()
+    {
+        allParticles = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    public void Play()
+    {
+        foreach (ParticleSystem ps in allParticles)
+        {
+            ps.Stop();
+            ps.Play();
+        }
+    }
+}
